55b86efa28227c62d9673de192b0bb1e8270be85
Fabio Moretti <fmoretti918@gmail.com>
2020-06-20 23:14:27 +0200

    removed config

--- src/App.js
+++ src/App.js
@@ -8,23 +8,7 @@ import { SyncedCloudDrawing } from "./SyncedCloudDrawing";
 import { MyMap } from "./MyMap";
 // import { relative } from "path";
 
-// var firebaseConfig = {
-//   apiKey: "AIzaSyATKb87cWl3HVu_ZhPjPyFcHR7NNp3IMpw",
-//   authDomain: "drawing-8e902.firebaseapp.com",
-//   databaseURL: "https://drawing-8e902.firebaseio.com",
-//   projectId: "drawing-8e902",
-//   storageBucket: "drawing-8e902.appspot.com",
-//   messagingSenderId: "1044099237885",
-//   appId: "1:1044099237885:web:f17a7457b552d309"
-// };
 var firebaseConfig = {
-  apiKey: "AIzaSyATKb87cWl3HVu_ZhPjPyFcHR7NNp3IMpw",
-  authDomain: "drawing-8e902.firebaseapp.com",
-  databaseURL: "https://drawing-8e902.firebaseio.com",
-  projectId: "drawing-8e902",
-  storageBucket: "drawing-8e902.appspot.com",
-  messagingSenderId: "1044099237885",
-  appId: "1:1044099237885:web:f17a7457b552d309"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
